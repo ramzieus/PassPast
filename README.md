e2566cc449cbd61ed4588972d55a3574b24ac62d
Ramzi Hemadou <ramzi.hemadou@flambosoft.com>
2018-12-22 14:21:56 +0100

    add the qr generator

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PassPast
 Generate strong password and copy it to clipboard
 very simple code
+
+
+## First Run
+gem install bundle
--- gp.rb
+++ gp.rb
@@ -2,12 +2,22 @@ require 'bundler/inline'
 gemfile do
   source 'https://rubygems.org'
   gem 'clipboard'
+  gem 'rqrcode'
 end
 
 require 'securerandom'
 require 'clipboard'
-password = SecureRandom.base64(32)
-puts password
-puts 'your password was coppied to clipboard'
-
+require 'rqrcode'
+print 'this password for : '
+a = gets.chomp.to_s
+print 'Enter the lenght of password : '
+b = gets.chomp.to_i
+password = SecureRandom.base64(b)
+puts '*'*b 
+puts 'your password was genarated'
 Clipboard.copy password
+puts 'your password was coppied to clipboard'
+qrcode = RQRCode::QRCode.new(password)
+image = qrcode.as_png
+IO.write(a+".png", image.to_s)
+puts 'The QR code was generated'
